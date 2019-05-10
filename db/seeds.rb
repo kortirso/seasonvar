3b0475e93d2d83f08fb29a80f7d9342566aaf366
kortirso <kortirso@gmail.com>
2019-05-10 15:34:03 +0200

    add seeds with moviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+movie_form = MovieForm.new(title: 'Game of thrones', plot: 'Battle between fire and ice')
+movie1 = movie_form.movie if movie_form.persist?
+
+season_form = SeasonForm.new(movie: movie1, number: 1)
+season1 = season_form.season if season_form.persist?
+
+episode_form = EpisodeForm.new(season: season1, number: 1)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 2)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 3)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 4)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 5)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 6)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 7)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 8)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 9)
+episode_form.persist?
+episode_form = EpisodeForm.new(season: season1, number: 10)
+episode_form.persist?
+
+movie_form = MovieForm.new(title: '300: Rise of an Empire', plot: 'Continue of 300 Spartans')
+movie_form.persist?
